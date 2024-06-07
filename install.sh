ae797049567c43a4a4e653fb8f5504fe98091f53
jdccccc <1704690357@qq.com>
2024-06-07 16:33:54 +0800

    wezterm , font and install scripts

---
+++ install.sh
@@ -0,0 +1,2 @@
+cp -r ./i3 ~/.config
+cp -r ./DejaVuSansMono /usr/share/fonts
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require("wezterm")
+local config = {
+	font_size = 14,
+	font = wezterm.font("DejaVuSansMNerdFontMono", { weight = "Regular" }),
+	color_scheme = "nord",
+
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	--window_decorations = "RESIZE",
+	show_new_tab_button_in_tab_bar = false,
+	macos_window_background_blur = 70,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
+
+return config
