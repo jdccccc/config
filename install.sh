cf2428c5bca64a436110e3856def00aab9c70d07
jdccccc <1704690357@qq.com>
2024-06-10 16:37:58 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 cp -r ./i3 ~/.config
 cp -r ./wezterm ~/.config
-cp -r ./font/DejaVuSansMono /usr/share/fonts
-cp -r ./font/wqy-bitmapsong /usr/share/fonts
+cp -r ./font/DejaVuSansMNerdFont /usr/share/fonts
