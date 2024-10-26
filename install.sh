694371ba1440e7133fdd702d8f1ef944953bbbac
jdccccc <1704690357@qq.com>
2024-10-26 10:34:03 +0800

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cp -r ./i3 /home/jdc/.config
 # cp ./i3blocks/config /etc/i3blocks.conf
 # cp -r ./rofi /home/jdc/.config
-cp -r ./wezterm ~/.config
+# cp -r ./wezterm ~/.config
 # cp -r ./font/DejaVuSansMNerdFont /usr/share/fonts
